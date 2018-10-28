3a220987fc72f767bcdc45d48ffc8e1f0acd5a28
lithin27 <44520546+lithin27@users.noreply.github.com>
2018-10-28 10:00:48 +0100

    poprawiłem poprzednie wciąrz html potrzebny
    6 graczy; 1 komputer; guziki naprawione wciąż bez kodu ale z kodem będą działać

---
+++ superstrategiajs1,1.js
@@ -0,0 +1,68 @@
+//wypisanie wszystkiego należy do HTML
+	var wybor;
+	var a;
+	var runda = 1;
+	var kolejka=0;
+	//gracze tabele inne niż w c++
+	var jedzenie = [100000, 100000, 100000, 100000, 100000, 100000];
+	var pieniadze = [100000, 100000, 100000, 100000, 100000, 100000];
+	var domy = [2, 2, 2, 2, 2, 2];
+	var farmy = [0, 0, 0, 0, 0, 0];
+	var sklepy = [0, 0, 0, 0, 0, 0];
+	var bazy_wojskowe = [0, 0, 0, 0, 0, 0];
+	var ludzie = [8, 8, 8, 8, 8, 8];
+	var zolnierze = [0, 0, 0, 0, 0, 0];
+	
+	while(true){
+	//wypisywanie potrzeba do tego już pliku html
+	for(int i=0; i<6;i++){
+	while(!/*kliknięto guzik*/){
+	
+			//gra gracz 1 html
+			
+    if /*klikniento guzik 1*/ {
+    	if(pieniadze[i] >= 20000)
+					{
+						domy[i]++;
+						ludzie[i] = ludzie[i] + 4;
+						pieniadze[i] = pieniadze[i] - 20000;
+					}
+    }else
+    if /*klikniento guzik 2*/{
+    	if(pieniadze[i] >= 50000 && ludzie[i] >= 5)
+					{
+						farmy[i]++;
+						ludzie[i] = ludzie[i] - 5;
+						pieniadze[i] = pieniadze[i] - 50000;
+					}
+    }else
+     if /*klikniento guzik 3*/{
+		if(pieniadze[i] >= 80000 && ludzie[i] >= 6)
+					{
+						sklepy[i]++;
+						ludzie[i] = ludzie[i] - 6;
+						pieniadze[i] = pieniadze[i] - 80000;
+					}
+     }else
+     if /*klikniento guzik 4*/{
+		if(pieniadze[i] >= 500000)
+					{
+						bazy_wojskowe[i]++;
+						pieniadze[i] = pieniadze[i] - 500000;
+					}
+     }else
+     if /*klikniento guzik 5*/{
+
+     }
+ }
+ }
+ 
+		for(int i=0;i<6;i++){
+			pieniadze[i] = pieniadze[i] + domy[i] * 1000 - farmy[i] * 2000 + sklepy[i] * 2500;
+			jedzenie[i] = jedzenie[i] - ludzie[i] * 2000 + farmy[i] * 20000;
+			}
+			//html odświerzenie
+		
+		runda++;
+	}
+
