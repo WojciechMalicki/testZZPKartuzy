75baf5a934a75c65323423a50ad47bf3cf36daf1
lithin27 <ikital27@gmail.com>
2019-01-26 19:58:42 +0100

    dodanie menu działa ale się rozwala
    trzeba jeszcze ponaprawiać aby się nie rozwalało

--- projekt.2/style dla guzikow + wydarzenia losowe (niedopracowane)/startPageScript.js
+++ projekt.2/style dla guzikow + wydarzenia losowe (niedopracowane)/startPageScript.js
@@ -1,1 +1,15 @@
-
+/*
+REVOEMAG STARTING PAGE'S SCRIPTS
+BY: ZzP 2018/2019 INF
+VERSION: 1.1*/
+let infosMenuButton = document.getElementById('infosMenuButton');
+let hrDividionTopAndMain = document.getElementById('hrDividionTopAndMain');
+let infosMenuSpan = document.getElementById('infosMenuSpan')
+function Dropdown() {
+  if (hrDividionTopAndMain.style.display === "none") {
+    hrDividionTopAndMain.style.display = "block";
+  }else{
+    hrDividionTopAndMain.style.display = "none";
+  }
+  infosMenuSpan.innerHTML='<ul><li>wikipedia</li><li>github repo</li><li>help</li><li>bug report</li><ul>'
+}
